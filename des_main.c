8a88be8d159306f56387b78ee9867d4bddfe9a89
slowjuggler <107360106+slowjuggler@users.noreply.github.com>
2022-07-22 11:26:36 +0300

    Update des_main.c

--- des_main.c
+++ des_main.c
@@ -4,19 +4,19 @@
 #include "des.h"
 
 int main() {
-	char input8[MAX_SIZE] = {0};
-	char key8[9] = {0};
-	printf("Please input string to encode: ");
-	input(input8);
-	int len = strlen(input8);
-	if (len%8 != 0) {
-		for (int i = 0; i < (8 - len%8); i++) {
-			*(input8 + len + i) = '0';
-		}
+    char input8[MAX_SIZE] = {0};
+    char key8[9] = {0};
+    printf("Please input string to encode: ");
+    input(input8);
+    int len = strlen(input8);
+    if (len%8 != 0) {
+	for (int i = 0; i < (8 - len%8); i++) {
+	    *(input8 + len + i) = '0';
 	}
-	*(input8 + len + 8 - len%8) = '\0';
-	printf("Please input 8-key (64 bit key block): ");
-	input(key8);
-	encode(input8, key8);
+    }
+    *(input8 + len + 8 - len%8) = '\0';
+    printf("Please input 8-key (64 bit key block): ");
+    input(key8);
+    encode(input8, key8);
     return 0;
 }
